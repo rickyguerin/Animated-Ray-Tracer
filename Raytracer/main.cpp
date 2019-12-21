4b8398e5a270910ee1c5cfa4a4d8e98a65d3628f
Ricky Guerin <Guerin.Ricky@gmail.com>
2019-12-21 12:36:05 -0500

    Use LodePNG to write image

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <lodepng.h>
+
+void writeImage(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
+	lodepng::encode(filename, image, width, height);
+}
 
 int main() {
-	int nx = 200;
-	int ny = 100;
 
-	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	unsigned width = 512, height = 512;
+	std::vector<unsigned char> image;
+	image.resize(width * height * 4);
+	for (unsigned y = 0; y < height; y++)
+		for (unsigned x = 0; x < width; x++) {
+			image[4 * width * y + 4 * x + 0] = 255 * !(x & y);
+			image[4 * width * y + 4 * x + 1] = x | y;
+			image[4 * width * y + 4 * x + 2] = x | y;
+			image[4 * width * y + 4 * x + 3] = 255;
+		}
 
-	return 0;
+	writeImage("../images/output.png", image, width, height);
 }
