f46db2288ebaea1e34450313eb8aaaf9f65eee55
Ricky Guerin <Guerin.Ricky@gmail.com>
2019-12-28 22:28:23 -0500

    Add Camera

---
+++ Raytracer/camera.cpp
@@ -0,0 +1,38 @@
+#include <glm/vec3.hpp>
+#include "image.cpp"
+
+class Camera {
+private:
+	// Position
+	glm::vec3 pos;
+	glm::vec3 lookat;
+	glm::vec3 up;
+
+	// Film Plane
+	unsigned width{ 256 };
+	unsigned height{ 256 };
+	float focalLength{ 1 };
+
+public:
+	Camera(glm::vec3 pos, glm::vec3 lookat, glm::vec3 up, unsigned width, unsigned height, float focalLength) {
+		this->pos = pos;
+		this->lookat = lookat;
+		this->up = up;
+
+		this->width = width;
+		this->height = height;
+		this->focalLength = focalLength;
+	}
+
+	void render(const char* filename) {
+		Image output(width, height);
+
+		for (unsigned y = 0; y < output.getHeight(); y++) {
+			for (unsigned x = 0; x < output.getWidth(); x++) {
+				output.setPixel(x, y, 255 * !(x & y), x | y, x | y, 255);
+			}
+		}
+
+		output.write(filename);
+	}
+};
