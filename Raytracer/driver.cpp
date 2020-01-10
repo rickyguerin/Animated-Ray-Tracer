d7a9a0425fd7fe23b9e9ea6cd9a48b64b2d295a2
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-01-09 21:48:44 -0500

    Create world in driver and send to render

--- Raytracer/driver.cpp
+++ Raytracer/driver.cpp
@@ -1,12 +1,23 @@
+#include <vector>
+#include "world.h"
 #include "camera.h"
+#include "sphere.h"
+
 
 int main() {
+	srand(420420);
+
+	Sphere leftSphere(glm::vec3(-2, 0, 5), 1);
+	Sphere rightSphere(glm::vec3(2, 0, 5), 1);
+
+	World world(std::vector<Sphere>{ leftSphere, rightSphere});
+
 	glm::vec3 pos{ 0, 0, 0 };
 	glm::vec3 lookat{ 0, 0, 1 };
 	glm::vec3 up{ 0, 1, 0 };
 
 	Camera camera(pos, lookat, up, 16, 9, 1.0f);
-	camera.render("../images/output.png", 960, 540);
+	camera.render(&world, "../images/output.png", 960, 540);
 
 	return 0;
 }
