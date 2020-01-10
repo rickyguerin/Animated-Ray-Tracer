597e1507641940804ffb466ee92e672b289ebd4f
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-01-09 22:09:37 -0500

    Implement basic collision for multiple spheres

--- Raytracer/world.cpp
+++ Raytracer/world.cpp
@@ -4,6 +4,18 @@ World::World(std::vector<Sphere> objects) {
 	this->objects = objects;
 }
 
-glm::vec3 World::trace(Ray ray) {
-	return glm::vec3(0, 0, 0);
+glm::ivec4 World::trace(Ray ray) {
+
+	for (int i = 0; i < objects.size(); i++) {
+		if (objects[i].collision(ray)) {
+			return glm::ivec4(255, 0, 0, 255);
+		}
+	}
+
+	if (double(rand()) / (double(RAND_MAX) + 1.0) < 0.001) {
+		return glm::ivec4(255, 255, 255, 255);
+	}
+	else {
+		return glm::ivec4(0, 0, 0, 255);
+	}
 }
