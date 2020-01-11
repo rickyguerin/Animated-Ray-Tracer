4049322bf83e73de2d37de41020b38a95191e0b8
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-01-11 10:41:27 -0500

    Implement parsing for sphere files

--- Raytracer/src/world.cpp
+++ Raytracer/src/world.cpp
@@ -21,9 +21,40 @@ glm::ivec4 World::trace(Ray ray, double time) {
 void World::add(std::string filename) {
 	std::ifstream inf(filename);
 
-	while (inf) {
-		std::string token;
-		inf >> token;
-		std::cout << token << "\n";
+	std::string token;
+
+	Sphere sphere;
+
+	inf >> token;
+	if (token.compare("SPHERE") == 0) {
+		while (inf) {
+			inf >> token;
+			if (token.compare("FRAME") == 0) {
+				inf >> token;
+				double timestamp = std::stod(token);
+
+				inf >> token;
+				double x = std::stod(token);
+
+				inf >> token;
+				double y = std::stod(token);
+				
+				inf >> token;
+				double z = std::stod(token);
+				
+				inf >> token;
+				double radius = std::stod(token);
+
+				SphereFrame sf {
+					timestamp,
+					glm::vec3(x, y, z),
+					radius
+				};
+
+				sphere.addFrame(sf);
+			}
+		}
+
+		objects.push_back(sphere);
 	}
 }
