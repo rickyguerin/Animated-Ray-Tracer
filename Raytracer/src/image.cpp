002b184d49a07754d27cd59ca69877d5491b1d2a
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-02-29 13:58:27 -0500

    Remove unused setPixel

--- Raytracer/src/image.cpp
+++ Raytracer/src/image.cpp
@@ -23,14 +23,6 @@ void Image::setPixel(long long x, long long y, glm::ivec4 color) {
 	contents[4 * lw * y + 4 * x + 3] = color.a;
 }
 
-void Image::setPixel(long long x, long long y, unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
-	long long lw = long long(width);
-	contents[4 * lw * y + 4 * x + 0] = r;
-	contents[4 * lw * y + 4 * x + 1] = g;
-	contents[4 * lw * y + 4 * x + 2] = b;
-	contents[4 * lw * y + 4 * x + 3] = a;
-}
-
 void Image::write(std::string filename) {
 	lodepng::encode(filename, contents, width, height);
 }
