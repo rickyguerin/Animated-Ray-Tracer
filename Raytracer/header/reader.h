bdad46222ac9740716c8c57f3ab531dcb6c34bf5
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-03-05 18:53:33 -0500

    Add more read functions

--- Raytracer/header/reader.h
+++ Raytracer/header/reader.h
@@ -3,10 +3,15 @@
 #define READER_H
 
 #include <fstream>
+#include <glm/glm.hpp>
 
 // Read one token from the file and convert to the provided type
-int readInt(const std::ifstream & file);
-float readFloat(const std::ifstream & file);
-double readDouble(const std::ifstream& file);
+int readInt(std::ifstream & file);
+float readFloat(std::ifstream & file);
+double readDouble(std::ifstream& file);
+
+// Read several tokens from the file and convert to a complex type
+glm::vec3 readPosition(std::ifstream & file);
+glm::ivec4 readColor(std::ifstream & file);
 
 #endif
--- Raytracer/src/reader.cpp
+++ Raytracer/src/reader.cpp
@@ -6,8 +6,51 @@
 // A string that every method can read into
 std::string token;
 
-// Read the next token of the file as an int
+// Read the next token as an int
 int readInt(std::ifstream & file) {
 	file >> token;
 	return std::stoi(token);
+}
+
+// Read the next token as a float
+float readFloat(std::ifstream & file) {
+	file >> token;
+	return std::stof(token);
+}
+
+// Read the next token as a double
+double readDouble(std::ifstream & file) {
+	file >> token;
+	return std::stod(token);
+}
+
+// Read the next three tokens as a vec3
+glm::vec3 readPosition(std::ifstream & file) {
+	file >> token;
+	float x = std::stof(token);
+
+	file >> token;
+	float y = std::stof(token);
+
+	file >> token;
+	float z = std::stof(token);
+
+	return glm::vec3(x, y, z);
+}
+
+// Read the next four tokens as a vec3
+glm::ivec4 readColor(std::ifstream & file) {
+	file >> token;
+	int r = std::stoi(token);
+
+	file >> token;
+	int g = std::stoi(token);
+
+	file >> token;
+	int b = std::stoi(token);
+
+	file >> token;
+	int a = std::stoi(token);
+
+	return glm::ivec4(r, g, b, a);
 }
