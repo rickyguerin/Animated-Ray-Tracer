12216c2a9b3ee7d080172eee7a36de97777ad159
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-03-05 18:05:04 -0500

    Create global token

--- Raytracer/src/reader.cpp
+++ Raytracer/src/reader.cpp
@@ -3,9 +3,11 @@
 
 #include "../header/reader.h"
 
-int readInt(std::ifstream & file) {
-	std::string token;
-	file >> token;
+// A string that every method can read into
+std::string token;
 
+// Read the next token of the file as an int
+int readInt(std::ifstream & file) {
+	file >> token;
 	return std::stoi(token);
 }
