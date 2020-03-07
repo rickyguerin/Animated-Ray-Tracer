b0743293e328de83db12926d78a87ee6957cdb50
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-03-07 10:56:30 -0500

    Add destructor

--- Raytracer/header/shapeProgram.h
+++ Raytracer/header/shapeProgram.h
@@ -10,6 +10,9 @@
 
 class ShapeProgram {
 public:
+	// Destructor
+	virtual ~ShapeProgram() {}
+
 	// Interpolate between frames to get the Shape at the given time
 	virtual Shape * getShape(const float time) const = 0;
 };
