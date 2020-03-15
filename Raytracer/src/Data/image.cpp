16700f2629c9941fde80b9d929aeaf836a2060e0
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-03-15 15:20:18 -0400

    convert floats to ints for lodepng

--- Raytracer/src/Data/image.cpp
+++ Raytracer/src/Data/image.cpp
@@ -8,9 +8,9 @@ Image::Image(const unsigned width, const unsigned height) {
 }
 
 void Image::setPixel(const unsigned x, const unsigned y, const glm::vec3& color) {
-	contents[4 * width * y + 4 * x + 0] = color.r;
-	contents[4 * width * y + 4 * x + 1] = color.g;
-	contents[4 * width * y + 4 * x + 2] = color.b;
+	contents[4 * width * y + 4 * x + 0] = color.r * 255;
+	contents[4 * width * y + 4 * x + 1] = color.g * 255;
+	contents[4 * width * y + 4 * x + 2] = color.b * 255;
 	contents[4 * width * y + 4 * x + 3] = 255;
 }
 
