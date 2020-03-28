c4ed3d85129966ff19240e377d7da24523913876
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-03-27 22:40:42 -0400

    Implement vec3 interpolate

--- Raytracer/header/Math/catmullRom.h
+++ Raytracer/header/Math/catmullRom.h
@@ -3,7 +3,20 @@
 #define INTERSECTION_H
 
 #include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
 
-glm::vec3 interpolate();
+static const float tension = 0.5f;
+
+static const glm::vec3 NULL_POINT = glm::vec3(9999.9f, 9999.9f, 9999.9f);
+
+static const glm::mat4x4 basis =
+	glm::mat4x4{
+		-tension, 2 * tension, -tension, 0,
+		3 * tension, -5 * tension, 0, 2 * tension,
+		-3 * tension, 4 * tension, tension, 0,
+		tension, -tension, 0, 0
+	};
+
+glm::vec3 interpolate(const float u, const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3);
 
 #endif
--- Raytracer/src/Math/catmullRom.cpp
+++ Raytracer/src/Math/catmullRom.cpp
@@ -1,5 +1,24 @@
+#include <glm/mat3x4.hpp>
 #include "../../header/Math/catmullRom.h"
 
-glm::vec3 interpolate() {
-	return glm::vec3();
+glm::vec3 interpolate(const float u, const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3) {
+
+	// Deal with initial/final point not existing
+	glm::vec3 first = p0;
+	glm::vec3 last = p3;
+
+	if (p0 == NULL_POINT) { first = p1; }
+	if (p3 == NULL_POINT) { last = p2; }
+
+	glm::vec4 U = glm::vec4 { pow(u, 3), pow(u, 2), u, 1 };
+
+	glm::mat3x4 G =
+		glm::mat3x4 {
+			first.x, p1.x, p2.x, last.x,
+			first.y, p1.y, p2.y, last.y,
+			first.z, p1.z, p2.z, last.z
+		};
+
+	glm::mat3x4 C = basis * G;
+	return U * C;
 }
