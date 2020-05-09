43be0f7dde207757603d6156c1d96bf8e426e6df
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-05-08 20:45:52 -0400

    Update driver.cpp

--- Raytracer/src/driver.cpp
+++ Raytracer/src/driver.cpp
@@ -25,7 +25,7 @@ std::string padInt(int i, int n) {
 
 int main() {
 	// Create the World
-	World world(glm::vec3(0.2f, 0.4f, 0.2f));
+	World world(glm::vec3(0.1f, 0.1f, 0.4f));
 
 	world.addProgram("world/noise/center.sphere");
 	world.addProgram("world/noise/main.light");
