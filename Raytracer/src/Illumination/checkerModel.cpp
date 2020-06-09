6128afcef9ff6761cfbda0d0265e00fd93945335
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-06-09 18:15:26 -0400

    add refIndex

--- Raytracer/src/Illumination/checkerModel.cpp
+++ Raytracer/src/Illumination/checkerModel.cpp
@@ -4,7 +4,7 @@
 
 CheckerModel::CheckerModel(const float checkSize, const glm::vec3& primaryColor, const glm::vec3& secondaryColor, const glm::vec3& specularColor,
 	const float ambientConst, const float diffuseConst, const float specularConst, const float specularExp,
-	const float kReflect, const float kRefract) : IlluminationModel(kReflect, kRefract) {
+	const float kReflect, const float kRefract, const float refIndex) : IlluminationModel(kReflect, kRefract, refIndex) {
 
 	this->checkSize = checkSize;
 	this->primaryColor = primaryColor;
@@ -63,6 +63,7 @@ IlluminationModel* CheckerModel::interpolate(IlluminationModel* other, const flo
 		glm::mix(this->specularConst, ((CheckerModel*)other)->specularConst, t),
 		glm::mix(this->specularExp, ((CheckerModel*)other)->specularExp, t),
 		glm::mix(this->kReflect, other->kReflect, t),
-		glm::mix(this->kRefract, other->kRefract, t)
+		glm::mix(this->kRefract, other->kRefract, t),
+		glm::mix(this->refIndex, other->refIndex, t)
 	);
 }
